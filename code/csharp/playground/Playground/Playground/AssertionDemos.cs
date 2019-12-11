96a78d6051703c25a199e749679c260241268a4a
Patrick Drechsler <patrick@pdrechsler.de>
2019-12-11 18:55:45 +0100

    code: add playground for showing nunit usage

---
+++ code/csharp/playground/Playground/Playground/AssertionDemos.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Playground
+{
+    [TestFixture]
+    public class AssertionDemos
+    {
+        [Test]
+        public void Classic_simple_string_comparison()
+        {
+            var result = "foo";
+            
+            // 1
+            Assert.IsTrue(result == "foox");
+            
+            // 2
+//            Assert.AreEqual(result, "foox");
+        }
+    }
+}
