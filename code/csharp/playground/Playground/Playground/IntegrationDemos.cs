d818213b1cbd2a786bdb0bcada64ffde7782dca0
Patrick Drechsler <patrick@pdrechsler.de>
2019-12-12 02:04:54 +0100

    integration tests: demo code

--- code/csharp/playground/Playground/Playground/IntegrationDemos.cs
+++ code/csharp/playground/Playground/Playground/IntegrationDemos.cs
@@ -6,16 +6,32 @@ namespace Playground
     [TestFixture]
     public class IntegrationDemos
     {
-        // TODO
+        // TODO adding new customer returns the number of customers
+        // TODO adding new customer returns new id
+        // TODO adding new customer returns new customer with id
     }
 
     public class CustomerChanger
     {
+        private readonly CustomerRepository _customerRepository;
+
+        public CustomerChanger()
+        {
+            _customerRepository = new CustomerRepository();
+        }
+
         void Add(Customer customer){}
     }
 
     public class CustomerProvider
     {
+        private readonly CustomerRepository _customerRepository;
+
+        public CustomerProvider()
+        {
+            _customerRepository = new CustomerRepository();
+        }
+
         Customer GetById(int customerId)
         {
             return new Customer
