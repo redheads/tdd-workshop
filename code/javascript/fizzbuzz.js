41e188b6983c42ccbd1c257e617148b17ce87781
Patrick Drechsler <patrick@pdrechsler.de>
2020-01-24 22:29:32 +0100

    code: javascript add comment about module system usage with vs-code jest plugin

--- code/javascript/fizzbuzz.js
+++ code/javascript/fizzbuzz.js
@@ -2,9 +2,14 @@ function greet(s) {
     return "Hello " + s;
 }
 
+// ES6 syntax (arrow notation)
+const greetArrow = (s) => "Test " + s;
+
 function fizzbuzz(number) {
     return "1";
 }
 
+// we use node's systemJs module notation, because it works out-of-the-box with VS-Code's "Jest" plugin
 exports.greet = greet;
+exports.greetArrow = greetArrow;
 exports.fizzbuzz = fizzbuzz;
--- code/javascript/fizzbuzz.test.js
+++ code/javascript/fizzbuzz.test.js
@@ -1,9 +1,14 @@
+// we use node's systemJs module notation, because it works out-of-the-box with VS-Code's "Jest" plugin
 const fizzbuzz = require('./fizzbuzz');
 
 describe ('Testing the test framework', () => {
     test('greet Homer Simpson', () => {
         expect(fizzbuzz.greet("Homer Simpson"))
             .toBe("Hello Homer Simpson");
+    })
+    test('greet using ES6 arrow notation', () => {
+        expect(fizzbuzz.greetArrow("Bart Simpson"))
+            .toBe("Test Bart Simpson");
     })
 })
 
